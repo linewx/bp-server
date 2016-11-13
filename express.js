79dabaabf60bfcdd286fd0f776553ca43528853a
linewx <linewx1981@gmail.com>
2016-11-13 19:03:56 +0800

    init bp server project

--- express.js
+++ express.js
@@ -54,7 +54,21 @@ app.delete('/collections/:collectionName/:id', function(req, res, next) {
   })
 })
 
+app.get('/api/bps/', function(req, res, next) {
+  res.send(bp);
+})
+
+app.post('/api/bp', function(req, res, next) {
+  bp.push(req.body);
+  res.send(bp);
+})
+
 app.listen(3000, function(){
   console.log('Express server listening on port 3000')
 })
+
+var bp = [{
+  low: 80,
+  height: 90
+}];
 
